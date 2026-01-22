cbaa940e4f22723d8530f5eea33f3cee1fd48168
quanhai27111998 <quanhai27111998@gmail.com>
2026-01-22 22:25:20 +0700

    Refactor: Extract amountFor method from statement

--- Main.java
+++ Main.java
@@ -116,6 +116,27 @@ class Customer {
 
         return result;
     }
+    
+        // Thêm hàm này vào lớp Customer
+private double amountFor(Rental each) {
+    double thisAmount = 0;
+    switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2)
+                thisAmount += (each.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3)
+                thisAmount += (each.getDaysRented() - 3) * 1.5;
+            break;
+    }
+    return thisAmount;
+}
 }
 
 // --- Lớp chạy chương trình ---
